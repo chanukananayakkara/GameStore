e685158701eb94c0e1e462a5e13b41f18b75fe5d
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2025-08-16 09:05:53 +0530

    created some games/data objects according to the resource representation to access them via an API

--- GamesStore.API/Program.cs
+++ GamesStore.API/Program.cs
@@ -1,5 +1,31 @@
+using GamesStore.API.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<GameDto> games = [
+    new(
+        1,
+        "Street Fiter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992,07,15)
+    ),
+    new(
+        2,
+        "Final Fantacy XIV",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2010,09,30)
+    ),
+    new(
+        3,
+        "FIFA 23",
+        "Sports",
+        69.99M,
+        new DateOnly(2022,09,27)
+    )
+];
 
 app.MapGet("/", () => "Hello World!");
 
