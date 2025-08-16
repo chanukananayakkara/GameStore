edc32eb15e6330b0c9eada4cdce1afdd739b82c2
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2025-08-16 21:37:16 +0530

    created the request and response to be send back to the user for the POST request

--- GamesStore.API/Program.cs
+++ GamesStore.API/Program.cs
@@ -3,6 +3,8 @@ using GamesStore.API.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const String GetGameEndpointName = "GetGame";
+
 List<GameDto> games = [
     new(
         1,
@@ -31,7 +33,23 @@ List<GameDto> games = [
 app.MapGet("games", () => games);
 
 //GET /games/1
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
+.WithName(GetGameEndpointName);
+
+//POST /games
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genere,
+        newGame.Price,
+        newGame.ReleaseDate);
+
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
 
 //initial sample request
 app.MapGet("/", () => "Hello World!");
