44f78e34e391244437e6a780dc3a5c8b4d66a4e2
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2025-08-21 19:23:34 +0530

    created an extension to store all the migrations in the database when application starts

--- GamesStore.API/Program.cs
+++ GamesStore.API/Program.cs
@@ -3,11 +3,13 @@ using GamesStore.API.Dtos;
 using GamesStore.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+
 
 var connString = builder.Configuration.GetConnectionString("GameStore");
 
 builder.Services.AddSqlite<GameStoreContext>(connString);
+
+var app = builder.Build();
 
 app.MapGamesEndpoints();
 
