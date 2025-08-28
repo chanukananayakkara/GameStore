435036a735ae762ce9772f2336e1793dec7afb4c
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2025-08-29 01:46:51 +0530

    created a seperate request file for request the genres

--- GamesStore.API/obj/Debug/net9.0/GamesStore.API.AssemblyInfo.cs
+++ GamesStore.API/obj/Debug/net9.0/GamesStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamesStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68a25820a53ab70cbe7108c2274f00ee3e7fac25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb264e46a441b08c2378ab806ce12ebbaa336f02")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamesStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamesStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
